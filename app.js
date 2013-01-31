98d41f2073330a7d8af8b5d14757693137f43997
gzmask <gzmask@gmail.com>
2013-01-31 22:31:54 +0000

    name changes

--- app.js
+++ app.js
@@ -34,15 +34,12 @@ ss.client.formatters.add(require('ss-stylus'));
 ss.client.templateEngine.use(require('ss-hogan'));
 
 // Minimize and pack assets if you type: SS_ENV=production node app.js
-if (ss.env === 'production') ss.client.packAssets();
+//ss.client.packAssets();
 
 // Start web server
 var server = http.Server(ss.http.middleware);
 
-if (ss.env === 'production')
-  server.listen(80);
-else
-  server.listen(3000);
+server.listen(80);
 
 // Start SocketStream
 ss.start(server);
